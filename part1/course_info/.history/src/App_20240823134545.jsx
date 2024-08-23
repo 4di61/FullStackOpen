936fafd1ab545e7b4b84daa9f5df6d75801efd87
4di61 <thisisadityajain@gmail.com>
2024-08-23 13:56:45 +0530

    part1 course-info exercise 1.1 added

---
+++ part1/course_info/.history/src/App_20240823134545.jsx
@@ -0,0 +1,43 @@
+const Header = (props)=>{
+  return (<h1>{props.course}</h1>)
+}
+
+const Content = (props)=>{
+  return (
+  <>
+    
+    <p>
+      {props.parts[0].name} {props.exercises1}
+    </p>
+    <p>
+      {props.part2} {props.exercises2}
+    </p>
+    <p>
+      {props.part3} {props.exercises3}
+    </p>
+  </>
+  )
+}
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+
+  return (
+    <div>
+      <Header course={course}/>
+      <Content/>
+      <Total/>
+      
+      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+    </div>
+  )
+}
+
+export default App
